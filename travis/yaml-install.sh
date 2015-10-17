5d24b4be305396767eff66c6b3bee2fd3a467a40
Bryan Davis <bd808@bd808.com>
2015-10-17 09:49:45 -0600

    Stop trying to use pecl to install

--- travis/yaml-install.sh
+++ travis/yaml-install.sh
@@ -8,32 +8,17 @@ set -e
 # Find the local ini file
 PHP_INI=$(php --ini | grep Loaded | awk '{print $NF}')
 
-if pecl &>/dev/null ; then
-  # PECL is present for this version of php
+# Download and unpack latest tarball
+curl -o yaml.tgz http://pecl.php.net/get/yaml
+tar xzf yaml.tgz
+cd yaml-*
 
-  pecl config-set php_ini $PHP_INI
-  printf "\n" | pecl install yaml-devel
+# standard pecl module build dance
+phpize
+./configure --with-yaml
+make
+printf "s\n" | make test
 
-else
-  # Travis doesn't always have pecl installed for alpha/beta php versions
-  # Do what pecl should do manually
-
-  echo "=================================================================="
-  echo "MANUAL INSTALL OF PECL MODULE"
-  echo " - Travis doesn't have a working pecl binary for ${TRAVIS_PHP_VERSION}"
-  echo "=================================================================="
-
-  # Download and unpack latest tarball
-  curl -o yaml.tgz http://pecl.php.net/get/yaml
-  tar xzf yaml.tgz
-  cd yaml-*
-
-  # standard pecl module build dance
-  phpize
-  ./configure --with-yaml
-  make
-
-  # install our new module
-  make install
-  echo "extension=yaml.so" >> $PHP_INI
-fi
+# install our new module
+make install
+echo "extension=yaml.so" >> $PHP_INI
